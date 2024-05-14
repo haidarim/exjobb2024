e937ac9cccd45cb9bcaedd80d95aef53e1e8b7ea
Mehdi Haidari <mehdi.haidari@initgroup.com>
2024-05-14 03:13:47 +0200

    changes

---
+++ ServerSim/ServerSim/communication/TCPCommunicator.cs
@@ -0,0 +1,194 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.Diagnostics.Contracts;
+using NUnit.Framework;
+using ServerSim.communication;
+
+namespace ServerSim.utils
+{
+    public class TCPCommunicator:ICommunicator
+    {
+
+        /// <summary>
+        /// extenablity: bradcasting
+        /// </summary>
+
+        #region privateVar
+        private RequestHandler RQHandler = new RequestHandler(); 
+        private volatile bool Running = false;
+        private string Ip;
+        private int Port;
+        private List<TcpClient> Clients; 
+        #endregion
+
+
+        public TCPCommunicator(string Ip, int Port)
+        {
+            if(string.IsNullOrEmpty(Ip) || string.IsNullOrWhiteSpace(Ip))
+            {
+                throw new ArgumentException("invalid ip address!");
+            }
+            if (Port < 0 || Port > 65535)
+            {
+                throw new ArgumentException("invalid port number!");
+            }
+            this.Ip = Ip;
+            this.Port = Port;
+            Clients = new List<TcpClient>();
+        }
+
+        public void Start()
+        {
+            if (Running)
+            {
+                throw new Exception("servre already running");
+            }
+            new Thread(()=>Run()).Start();            
+        }
+
+        private void Run()
+        {
+            try
+            {
+                using (TcpListener server = new TcpListener(IPAddress.Parse(this.Ip), this.Port))
+                {
+                    // Start the server 
+                    server.Start();
+                    this.Running = true;
+
+                    Console.WriteLine("TCP-Server: Väntar på anslutning...");
+
+                    while (this.Running)
+                    {
+                        // accept connections 
+                        TcpClient client = server.AcceptTcpClient();
+
+                        Clients.Add(client);
+                        Console.WriteLine("Ansluten!");
+
+                        // new connection has its own thread 
+                        Thread ClientThread = new Thread(() => RunClientHandler(client));
+                        ClientThread.Start();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"failure in server-setup: {e.Message}");
+                throw new Exception("server failed!");
+            }
+        }
+
+        public void Stop()
+        {
+            this.Running = false;
+            foreach (TcpClient Client in Clients)
+            {
+                if (Client != null)
+                {
+                    if (Client.Client != null && Client.Client.Connected)
+                    {
+                        Client.Client.Shutdown(SocketShutdown.Both);
+                    }
+                    Client.Close();
+                }
+            }
+
+            Clients.Clear();
+        }
+
+        public bool IsRunning()
+        {
+            return this.Running;
+        }
+
+ 
+
+        public void RemoveClient(TcpClient Client)
+        {
+            if (Clients.Contains(Client) && !Clients.Remove(Client))
+            {
+                throw new Exception($"ContractFailedEventArgs to remove the client");
+            }
+            Client.Close();
+        }
+
+ 
+
+        private void RunClientHandler(TcpClient client)
+        {
+            /*
+            try
+            {
+                using (NetworkStream clientIOStream = client.GetStream())
+                using (BufferedStream bufferedStream = new BufferedStream(clientIOStream))
+                using (StreamReader reader = new StreamReader(bufferedStream))
+                using (StreamWriter writer = new StreamWriter(bufferedStream, Encoding.UTF8))
+                {
+                    // read data 
+                    var readed = reader.ReadLine();
+ 
+                    if (readed != null)
+                    {
+                        string request = readed.ToString();
+
+                        string response = reqHandler.HandleRequest(request).Replace("\n", " ").Replace("\r", " ");
+                        // send the response 
+                        writer.WriteLine(response);
+                        writer.Flush(); // töm bufferten och skicka data omedelbart
+                        Console.WriteLine("Response sent!");
+                    }
+                }
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine($"failure when running client handler RunClientHandler(...): {e.Message}");
+            }*/
+            try
+            {
+
+
+
+                //StreamReader reader = new StreamReader(clientStream);
+                //StreamWriter writer = new StreamWriter(clientStream, Encoding.UTF8);
+                using (NetworkStream clientStream = client.GetStream())
+                using (BufferedStream bufferedStream = new BufferedStream(clientStream))
+                using (StreamReader reader = new StreamReader(bufferedStream))
+                using (StreamWriter writer = new StreamWriter(bufferedStream, Encoding.UTF8))
+                {
+                    string? request;
+                    while ((request = reader.ReadLine()) != null) {
+                        Console.WriteLine("\n\n readed\n\n");
+                        string response = RQHandler.HandleRequest(request).Replace("\n", " ").Replace("\r", " ");
+
+                        writer.WriteLine(response);
+                        writer.Flush(); // Flush the buffer and send data immediately
+                        Console.WriteLine("Response sent!");
+                    }
+                    Console.WriteLine("Client disconnected. \"#########\"");
+                }
+
+                
+                RemoveClient(client);
+                Console.WriteLine("Removed");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failure when running client handler: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("closed the client.Socket!");
+            }
+        }
+
+        
+        
+
+
+    }
+}
---
+++ javaClient/src/WindowsPipeCommunicator.java
@@ -0,0 +1,124 @@
+package src;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.concurrent.*;
+
+
+/**********************************************************************************************************************************************************************************************************
+ * This class makes it possible to communicate with an external process running locally in same Windows OS.                                                                                               *
+ * The communication is established through two named pipes, one for writing and other one for reading                                                                                                    *
+ * The named pipes are not created here, rather this class only use them to issue requests.                                                                                                               *
+ * The {@code WindowsPipeCommunicator.sendRequest(String req, boolean timeOn, int maxTimeInSec)} sends a request and wait for the response.                                                               *
+ * If timerOn is true then the method waits until the maximum waiting time is up and returns an error message if no pipes available, otherwise (!timerOn) the method waits until pipes are available.     *
+ * This method is synchronized                                                                                                                                                                            *
+ * @author Mehdi Haidari                                                                                                                                                                                  *
+ * @author Theodor Ahlgren                                                                                                                                                                                *
+ * ********************************************************************************************************************************************************************************************************/
+public class WindowsPipeCommunicator {
+
+
+    /**********************************************************************************************************************************************************
+     * @param req which is a String, indicates the desired data                                                                                               *
+     * @param timerOn is a boolean indicating whether this method should return after maximum waiting time is up                                              *
+     * @param maxTimeInSec is an integer indicating the maximum waiting time for pipe to be available                                                         *
+     * @return String response to the requestedData, if the maximum waiting time has been reached and timer was on this method return a time-out message      *
+     * @throws IllegalArgumentException if req == null || req.isEmpty || req.isBlank                                                                          *
+     * ********************************************************************************************************************************************************/
+    public static synchronized String sendRequestByPipe(String req, boolean timerOn, int maxTimeInSec) {
+        // the input should be valid
+        if (req == null ||req.isBlank()) {
+            throw new IllegalArgumentException("invalid input!");
+        }
+        //boolean to be used if maximum waiting time is up
+        boolean[] timeOut = new boolean[1];
+        // create single executor managing time-out task
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        Future<?> oneShotAction = new CompletableFuture<>();
+        if(timerOn){
+            oneShotAction = executor.schedule(()-> {
+                timeOut[0] = true;
+                executor.shutdown();
+            }, maxTimeInSec, TimeUnit.SECONDS);
+        }
+
+        while (true){
+            // check if max time is up
+            if(timeOut[0]){
+                return "waiting time is up! please run pipe server and try again!";
+            }
+            // check if pipes are available
+            if(pipeExists("writePipe") && pipeExists("readPipe")){
+                oneShotAction.cancel(true); // cancel the timer functionality
+                executor.shutdown(); // shut down the timer thread
+                break;
+            }
+        }
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get("\\\\.\\pipe\\readPipe"))) { // writing stream which writes to the reading-end of the server
+            String cleanedRequest = req.replaceAll("\n", " ").replaceAll("\r", " ");
+
+            writer.write(cleanedRequest); // write the request
+            writer.newLine(); // indicate end of the line
+            writer.flush(); // flush the pipe
+            writer.close(); // close, not needed any more
+            try( BufferedReader reader = Files.newBufferedReader(Paths.get("\\\\.\\pipe\\writePipe"))) { // reading stream which listens/reads from the writing-end of the server
+                String response = reader.readLine();
+
+                reader.close(); // close the reading end, not needed any more
+                return response; // return the response for the actual thread
+            }catch (IOException e){
+                printError("failed to request data (while trying to get the response)", e.getMessage());
+                return "failed to request the data";
+            }
+        } catch (IOException e) {
+            printError("failed to request data", e.getMessage());
+            return "failed to request the data";
+        }
+    }
+
+    /**********************************************************************************
+     * @param pipeName String, name of the pipe to check if exists                    *
+     * @return  boolean, true if pipe exists in pipe directory otherwise false        *
+     * ********************************************************************************/
+    public static boolean pipeExists(String pipeName){
+        boolean exists = false;
+        FileSystem fs = FileSystems.getDefault();
+        Path directory = fs.getPath("\\\\.\\pipe"); // Path to map where all pipes exist in Windows
+        try (DirectoryStream<Path> paths = Files.newDirectoryStream(directory)) {
+            for (Path p : paths) {
+                if (p.toString().contains(pipeName)) {
+                    exists = true;
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            printError("failed to check if pipe exists", e.getMessage());
+            return false;
+        }
+        return exists;
+    }
+
+
+    // error printer used internally
+    private  static void printError(String message, String err){
+        System.out.println(
+                "Err! "
+                + message +"\n"
+                + err
+        );
+    }
+}
+
+
+
+
+/*
+ *
+ * challenges:
+ *  1) multi and sequential request
+ *  2) problem was when pipe was not available, the synchronized block worked correctly otherwise
+ *  3) why I implemented my own method to check pipe's existence
+ * */
